b152e57bbbfd91a6d25d14a7b8454816f718cf5f
Yemini <yeminimatan@gmail.com>
2020-05-08 19:12:42 +0300

    Cloud Integration and Errors Infra

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { json } from 'body-parser';
-
-import authRouter from './routes/auth';
+import { authRouter } from './routes/auth';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 
 app.use(json());
 
 app.use('/api/users', authRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('running on port 3000!!');
