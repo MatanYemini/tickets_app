5a18c45caacafcddc3a6f712f4b2e6e3fbb15046
Yemini <yeminimatan@gmail.com>
2020-05-19 11:24:42 +0300

    Adding NATS streamer deployment and code

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,36 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  stan.on('close', () => {
+    console.log('NATS listener closed');
+    process.exit();
+  });
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'order-service-queue-group',
+    options
+  );
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+  });
+});
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,22 @@
+import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('publisher is connected to NATS');
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
+
+  stan.publish('ticket:created', data, () => {
+    console.log('event published');
+  });
+});
