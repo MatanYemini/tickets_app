a609e6969e1b348c93182a7bffee086689d3265c
Yemini <yeminimatan@gmail.com>
2020-05-23 13:15:46 +0300

    Adding order service routes

--- nats-test/src/events/subjects.ts
+++ nats-test/src/events/subjects.ts
@@ -1,4 +1,6 @@
 export enum Subjects {
   TicketCreated = 'ticket:created',
-  OrderUpdated = 'order:updated',
+  TicketUpdated = 'ticket:updated',
+  OrderCreated = 'order:created',
+  OrderCancelled = 'order:cancelled',
 }
